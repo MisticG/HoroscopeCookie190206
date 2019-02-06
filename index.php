9a22ef318d93a9a75d38f298b752279d2fd880e7
misticG <tuy-vi.huynh@medieinstitutet.se>
2019-02-06 11:21:16 +0100

    init commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <h4>Förnamn: </h4><input type="text" name="firstName">
+        <h4>Efternamn: </h4> <input type="text" name="lastName">
+        <h4>Personnummer: </h4> <input type="text" name="firstName">
+        <br>
+        <input type="submit" value="Klicka här!">
+    </form>
+    
+</body>
+</html>
